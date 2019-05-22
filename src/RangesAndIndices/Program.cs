a934e11f34e214348f72f293c22dd4e6a63f50f7
pawel.rzeczkowski <pawel.rzeczkowski@stepstone.com>
2019-05-22 08:02:29 +0100

    ranges and indices

--- src/RangesAndIndices/Program.cs
+++ src/RangesAndIndices/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RangesAndIndices
 {
@@ -6,7 +7,23 @@ namespace RangesAndIndices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Index x = 3;       // number 3 from beginning
+            Index y = ^4;      // number 4 from end
+            Console.WriteLine($"index results: {a[x]}, {a[y]}");
+
+            var slice = a[x..y];
+            Console.WriteLine($"range slice results: {string.Join(",", slice)}");
+
+            var rangeAll = a[..];
+            Console.WriteLine($"range all results: {string.Join(",", rangeAll)}");
+
+            var last = ^1; // simpler compared to using arr.Length - 1 to access the last item
+            var rangeToLast = a[..last];
+            Console.WriteLine($"range first to last results: {string.Join(",", rangeToLast)}");
+
+            Console.ReadKey();
         }
     }
 }
