c5a44c42d39f7b9603804502c2526387444de2b1
pawel.rzeczkowski <pawel.rzeczkowski@stepstone.com>
2019-05-23 15:34:23 +0100

    default interface implementations

--- src/DefaultImplementations/Program.cs
+++ src/DefaultImplementations/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+// https://www.codejourney.net/2019/02/csharp-8-default-interface-methods/
 namespace DefaultImplementations
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    //interface IDeveloper {
+    //    void LearnNewLanguage(string language)
+    //    {
+    //        Console.Write($"Learning {language} in a default way.");
+    //    }
+    //}
+    //interface IBackendDev : IDeveloper {
+    //    void LearnNewLanguage(string language)
+    //    {
+    //        Console.Write($"Learning {language} in a backend way.");
+    //    }
+    //}
+    //interface IFrontendDev : IDeveloper {
+    //    void LearnNewLanguage(string language)
+    //    {
+    //        Console.Write($"Learning {language} in a frontend way.");
+    //    }
+    //}
+    //interface IFullStackDev : IBackendDev, IFrontendDev
+    //{
+    //    // uses default implementation,
+    //    // of LearnNewLanguage, but which one?	  class Dev : IFullStackDev { }
+    //}
+
+    //class Dev : IFullStackDev { }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        IFullStackDev dev = new Dev();
+    //        dev.LearnNewLanguage("TypeScript");
+    //    }
+    //}
 }
