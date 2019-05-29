b87a189fe4a0dd1f27d3f319b9cb5061d99116bb
pawel.rzeczkowski <pawel.rzeczkowski@stepstone.com>
2019-05-29 16:08:03 +0100

    switch expressions

---
+++ src/SwitchExpressions/MyEnums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitchExpressions
+{
+    public class MyEnums
+    {
+        public enum State
+        {
+            Running, Suspended, NotRunning
+        };
+
+        public enum Transition
+        {
+            Suspend, Resume, Terminate, Activate
+        };
+
+        public enum Ignition
+        {
+            On, Off
+        };
+    }
+}
--- src/SwitchExpressions/Program.cs
+++ src/SwitchExpressions/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using static SwitchExpressions.MyEnums;
 
 namespace SwitchExpressions
 {
     class Program
     {
+        private static Transition transition = Transition.Resume;
+        private static State state = State.Suspended;
+        private static Ignition ignition = Ignition.On;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tupleSwitch();
+            Console.ReadKey();
+
+            switchExpression();
+            Console.ReadKey();
+        }
+
+        static void tupleSwitch()
+        {
+            Console.WriteLine($"initial state: {Enum.GetName(typeof(State), state)}");
+
+            switch (state, transition, ignition)
+            {
+                case (State.Running, Transition.Suspend, Ignition.On):
+                    state = State.Suspended;
+                    break;
+                case (State.Suspended, Transition.Resume, Ignition.On):
+                    state = State.Running;
+                    break;
+                case (State.Suspended, Transition.Terminate, Ignition.Off):
+                    state = State.NotRunning;
+                    break;
+                case (State.NotRunning, Transition.Activate, Ignition.On):
+                    state = State.Running;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+
+            }
+
+            Console.WriteLine($"state after tuple switch: {Enum.GetName(typeof(State), state)}");
+        }
+
+        static void switchExpression()
+        {
+            state = State.Suspended;
+            transition = Transition.Terminate;
+            ignition = Ignition.Off;
+
+            Console.WriteLine($"initial state: {Enum.GetName(typeof(State), state)}");
+
+            state = (state, transition, ignition) switch
+            {
+                (State.Running, Transition.Suspend, Ignition.On) => State.Suspended,
+                (State.Suspended, Transition.Resume, Ignition.On) => State.Running,
+                (State.Suspended, Transition.Terminate, Ignition.Off) => State.NotRunning,
+                (State.NotRunning, Transition.Activate, Ignition.On) => ,
+                _ => throw new InvalidOperationException()
+            };
+
+            Console.WriteLine($"state after tuple switch: {Enum.GetName(typeof(State), state)}");
         }
     }
 }
