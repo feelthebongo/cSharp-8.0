1602788024b1dc4e691d8a1127f0c248cc2cf47b
pawel.rzeczkowski <pawel.rzeczkowski@stepstone.com>
2019-06-03 16:46:49 +0100

    async streams git add

---
+++ src/AsyncStreams/Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    public class Service
+    {
+        public static IEnumerable<Candidate> GetCandidates()
+        {
+            var x = new[] {
+                new Candidate("Lady", "Sansa", "Stark"),
+                new Candidate("Knight", "Lady", "Brianne"),
+                new Candidate("Denerys", "MotherOfDragons", "Targaryen"),
+                new Candidate("Jon", "ThisIsMyQueen", "Snow"),
+                new Candidate("Aria", "Stark")
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                yield return x[i];
+            }
+        }
+    }
+}
